88a5bda9aa9708627afd7d224d8492c76384b9cf
xingyuzhang65-cloud <xingyuzhang65@gmail.com>
2026-07-23 17:52:05 +0800

    Configure tiantu-next Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/tiantu-----/',
+    base: '/tiantu-next/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
